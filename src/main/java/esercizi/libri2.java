a7a29eeb856cdbb501babc7c043d754eeb5677a6
jbaskara <jbaskara@studenti.apuliadigitalmaker.it>
2025-05-06 11:17:23 +0200

    Esercizi

--- src/main/java/esercizi/libri2.java
+++ src/main/java/esercizi/libri2.java
@@ -1,45 +1,28 @@
 package esercizi;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-public class libri {
+public class libri2 {
 	
 	public static void main(String[] args) {
 		
-		List<String> libri = new ArrayList<>();
+		Set<String> libri = new HashSet<>();
 		
 		// add(element) - aggiunge un elemento
 		libri.add("Il Signore degli Anelli");
 		libri.add("1984");
 		libri.add("Il Piccolo Principe");
 		System.out.println("Lista libri: " + libri);
-		
-		// sort() - ordina la lista
-		Collections.sort(libri);
-		System.out.println("Lista ordinata: " + libri);
 		System.out.println();
 		
 		// add(index, element) - inserisce a una certa posizione
-		libri.add(1, "Harry Potter");
+		libri.add("Harry Potter");
 		System.out.println("Libro Harry Potter aggiunto.");
 		System.out.println("Lista libri: " + libri);
 		System.out.println();
 		
-		// get(index) - prende un elemento
-		System.out.println("Libro in posizione 2: " + libri.get(2));
-		
-		// set(index, element) - modifica un elemento
-		libri.set(2, "Titanic");
-		System.out.println("Libro Il Piccolo Principe sostituito con Titanic");
-		System.out.println("Lista libri: " + libri);
-		System.out.println();
-		
-		// remove(index/obj) - rimuove un elemento
-		libri.remove(0);
 		libri.remove("Harry Potter");
-		System.out.println("Libro 1984 rimosso.");
 		System.out.println("Libro Harry Potter rimosso.");
 		System.out.println();
 		
@@ -55,10 +38,6 @@ public class libri {
 		// size() - numero di elementi
 		System.out.println("Numero di libri: " + libri.size());
 		System.out.println("Lista finale: " + libri);
-		
-		// reverse() - inverte la lista
-		Collections.reverse(libri);
-		System.out.println("Lista invertita: " + libri);
 		
 	}
 
